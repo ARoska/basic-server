0d84f473b7c034dc64b7c7ea05c07267d9a1a85e
Andrew Roska <andrew.l.roska@gmail.com>
2019-02-15 17:00:13 -0800

    Initializes server

---
+++ public/README.md
@@ -0,0 +1,24 @@
+
+```
+Number and name of feature: Feature #1: Refactor the CSS
+
+Estimate of time needed to complete: 2 hours
+
+Start time: 10am
+
+Finish time: 12:03pm
+
+Actual time needed to complete: 2 hours
+```
+
+```
+Number and name of feature: Feature #2: Modify the contents
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 12:10pm
+
+Finish time: 1:20pm
+
+Actual time needed to complete: 1 hour 10 min 
+```
---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+// Load dotenv to manage veriables
+require('dotenv').config();
+
+// Load express to do the heavy lifting of the server
+const express = require('express');
+const app = express();
+
+// Establish the PORT number
+const PORT = process.env.PORT || 3000;
+
+// Tell Express where to load our HTML files from
+app.use(express.static('./public'));
+
+// Creat routes/paths that the user can access the server from
+app.get('/hello', (request, response) => {
+  response.status(200).send('Hello');
+});
+
+app.get('/data', (request, response) => {
+  let airplanes = {
+    departure: Date.now(),
+    canFly: true,
+    pilot: 'Well Trained'
+  }
+
+  response.status(200).json(airplanes);
+});
+
+app.get('/', (request, response) => {
+  response.status(200).redirect('index.html');
+});
+
+// Added catch-all to get routes that don't exist
+app.use('*', (request, response) => response.send(`Sorry, that route does not exist`));
+
+// Turn server on
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
